d444c2c671864ed90fb6dc3344fa9145213aab79
ShacoDeBigode <omardotaxi@gmail.com>
2026-03-23 09:54:33 -0300

    Controle de Acesso funcional. CheckIn realizado apenas com a mensalidade em dia

--- gestao-academia/src/main/java/com/gestao_academia/service/CheckInService.java
+++ gestao-academia/src/main/java/com/gestao_academia/service/CheckInService.java
@@ -1,18 +1,34 @@
 package com.gestao_academia.service;
+
 import com.gestao_academia.model.CheckIn;
+import com.gestao_academia.model.Mensalidade;
 import com.gestao_academia.repository.CheckInRepository;
+import com.gestao_academia.repository.MensalidadeRepository; // Importa o de mensalidade!
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
 
-
 @Service
-public class CheckInService {
+public class CheckInService{
+
     @Autowired
     private CheckInRepository repository;
 
-    public CheckIn registrar(CheckIn checkIn){
+    @Autowired
+    private MensalidadeRepository mensalidadeRepository;
+
+    public CheckIn registrar(CheckIn checkIn) {
+        List<Mensalidade> mensalidades = mensalidadeRepository.findByAlunoId(checkIn.getAluno().getId());
+
+        boolean temDivida = mensalidades.stream()
+                .anyMatch(m ->"PENDENTE".equals(m.getStatus()));
+
+        if (temDivida) {
+            throw new RuntimeException("Acesso negado! Aluno com mensalidade pendente");
+        }
         return repository.save(checkIn);
+
     }
 
     public List<CheckIn> listarTodos(){
